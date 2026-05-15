98a18484528f1f4dc40434aed48c068738d83289
Isaev34 <isaevelmar34@gmail.com>
2026-05-15 16:44:39 +0300

    описание что изменил

--- users/migrations/0011_manager_promo_permissions.py
+++ users/migrations/0011_manager_promo_permissions.py
@@ -2,9 +2,12 @@ from django.db import migrations
 
 
 def sync_manager_promo_permissions(apps, schema_editor):
+    # Используем «живые» модели: get_manager_permissions() возвращает Permission,
+    # а historical Group из apps.get_model несовместим с ними при .permissions.set().
+    from django.contrib.auth.models import Group
+
     from users.roles import GROUP_MANAGER, get_manager_permissions
 
-    Group = apps.get_model("auth", "Group")
     manager_group = Group.objects.filter(name=GROUP_MANAGER).first()
     if not manager_group:
         return
